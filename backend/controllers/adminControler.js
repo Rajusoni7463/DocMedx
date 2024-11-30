6eb2de33d3efc5f32551eb89786d9acfff24ba96
Raju <soniraju13344@gmail.com>
2024-11-30 12:16:14 +0530

    just complete the admin login and addDoctor

--- backend/controllers/adminControler.js
+++ backend/controllers/adminControler.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from "../models/doctorModel.js";
+import jwt from 'jsonwebtoken'
 
 
 //Api for adding doctor
@@ -60,4 +61,27 @@ const addDoctor = async (req,res) =>{
 
     }
 }
-export {addDoctor};
+
+// API for admin login
+const loginAdmin = async (req,res)=>{
+
+    try{
+        const {email,password} = req.body;
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+
+
+        }else{
+            res.json({success:false,message:"invalid credentials"})
+
+        }
+
+    }
+    catch(e){
+        console.log(e)
+        res.json({success:false,message:e.message})
+
+    }
+}
+export {addDoctor,loginAdmin};
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addDoctor } from '../controllers/adminControler.js'
+import { addDoctor, loginAdmin} from '../controllers/adminControler.js'
 import upload from '../middleware/multer.js'
 
 const adminRouter = express.Router()
 
 adminRouter.post('/add_doctor',upload.single('image'),addDoctor)
+adminRouter.post('/login',loginAdmin)
 
 export default adminRouter;
